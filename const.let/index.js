8d3eb511755c08a7ea691eac79e2d794fa36a90e
leonneto <leonardobatista661@gmail.com>
2023-05-18 18:25:51 -0400

    estudos javascript

---
+++ const.let/index.js
@@ -0,0 +1,13 @@
+/*
+LUIZ OTÁVIO MIRANDA TEM 30 ANOS, PESA 84KG
+TEM 1.8 DE ALTURA E SEU IMC É DE 25.92
+*/
+
+const nome= 'Luiz'
+const sobrenome= 'Otávio Miranda'
+const idade= 30;
+const peso= 84;
+const altura= 1.8;
+let IMC= peso/(altura*altura);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} tem ${altura} de altura e seu IMC é de ${IMC}`);
