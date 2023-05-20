a02aa9998c0138ad52ff82bafb9799510b4dceea
Leonardo Batista de Souza Neto <107722215+leonneto@users.noreply.github.com>
2023-05-20 16:25:32 -0400

    Add files via upload

---
+++ Operadores aritmeticos, de atribuiçao,incremento e alert,confirm e prompt/index.js
@@ -0,0 +1,5 @@
+let num1= Number(window.prompt("Digite o valor do primeiro numero:"));
+let num2= Number(window.prompt("Digite o valor do segundo numero:"));
+const soma= num1 + num2;
+
+window.alert("Resultado da soma:"+ soma);
