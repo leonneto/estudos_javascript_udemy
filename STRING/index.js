5b69286a3226bfcdcb6cf5d67ac6dacdda78bcdc
Leonardo Batista de Souza Neto <107722215+leonneto@users.noreply.github.com>
2023-05-22 17:10:50 -0400

    Add files via upload

---
+++ STRING/index.js
@@ -0,0 +1,8 @@
+let umaString = 'Um texto';
+
+
+//console.log(umaString.charAt(5)); procruar o index
+//console.log(umaString.indexOf('text'));descubrir a onde começa o index da palavra
+//console.log(umaString.replace('Um','talvez'))
+  //console.log(umaString.length); tamanho da string
+  console.log(umaString.slice(2,6))
